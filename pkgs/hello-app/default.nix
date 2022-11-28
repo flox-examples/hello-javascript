a6d7faa9c2bb2ee040fb8fe6d462f6554928894d
Tom Bereknyei <tomberek@gmail.com>
2022-11-28 15:26:27 -0500

    init: d2n

--- pkgs/hello-app/default.nix
+++ pkgs/hello-app/default.nix
@@ -1,7 +1,39 @@
-{self, mkYarnPackage}: 
-mkYarnPackage {
-  name = "hello-app";
-  src = self;
-  packageJson = self + "/package.json";
-  yarnLock = self + "/yarn.lock";
-}
+{self, system, inputs}:
+
+let
+  # This can be a builder
+  mkDream = {system, src, inputs, ...}: 
+# {{{
+  let
+   l = inputs.d2n.inputs.nixpkgs.lib // builtins;
+   pkgs = inputs.d2n.inputs.nixpkgs.legacyPackages.${system};
+
+   initD2N = pkgs:
+inputs.d2n.lib.init {
+        inherit pkgs;
+        config.projectRoot = ../..;
+      };
+
+   makeOutputs = pkgs: let
+      outputs = (initD2N pkgs).dream2nix-interface.makeOutputs {
+        source = self;
+        settings = [
+          {
+            builder = "granular-nodejs";
+            translator = "package-lock";
+          }
+        ];
+      };
+    in rec {
+      packages.${pkgs.system} = outputs.packages;
+    };
+  outputs = makeOutputs pkgs;
+in
+  outputs.packages.${system}.default;
+in  # }}}
+
+  mkDream {
+    inherit system inputs;
+    src = self;
+  }
+

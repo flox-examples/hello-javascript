7b56aa7b10dc6e0f4b149c6ef1c0533d2b084a76
Tom Bereknyei <tomberek@gmail.com>
2022-11-28 15:41:58 -0500

    fix: cleanup builder

--- pkgs/hello-app/default.nix
+++ pkgs/hello-app/default.nix
@@ -1,39 +1,44 @@
-{self, system, inputs}:
-
+{
+  self,
+  system,
+  inputs,
+  pkgs,
+}:
 let
   # This can be a builder
-  mkDream = {system, src, inputs, ...}: 
-# {{{
+  mkDream = args @ { # {{{
+    pkgs,
+    src,
+    inputs,
+    ...
+  }:
   let
-   l = inputs.d2n.inputs.nixpkgs.lib // builtins;
-   pkgs = inputs.d2n.inputs.nixpkgs.legacyPackages.${system};
+    makeOutputs = pkgs: let
+      initD2N = pkgs:
+        inputs.d2n.lib.init {
+          inherit pkgs;
+          config.projectRoot = src;
+        };
 
-   initD2N = pkgs:
-inputs.d2n.lib.init {
-        inherit pkgs;
-        config.projectRoot = ../..;
-      };
-
-   makeOutputs = pkgs: let
-      outputs = (initD2N pkgs).dream2nix-interface.makeOutputs {
-        source = self;
-        settings = [
-          {
-            builder = "granular-nodejs";
-            translator = "package-lock";
-          }
-        ];
-      };
-    in rec {
-      packages.${pkgs.system} = outputs.packages;
-    };
-  outputs = makeOutputs pkgs;
+      outputs = (initD2N pkgs).dream2nix-interface.makeOutputs ({
+          source = src;
+          settings = [
+            {
+              builder = "granular-nodejs";
+              translator = "package-lock";
+            }
+          ];
+        }
+        // (builtins.removeAttrs args ["src" "inputs" "pkgs"]));
+    in
+      outputs.packages.default;
+    outputs = makeOutputs pkgs;
+  in
+    makeOutputs pkgs;
 in
-  outputs.packages.${system}.default;
-in  # }}}
-
+  # }}}
   mkDream {
-    inherit system inputs;
+    inherit inputs pkgs;
     src = self;
+    # packageOverrides = {};
   }
-

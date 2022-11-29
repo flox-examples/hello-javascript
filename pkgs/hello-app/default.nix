82af8a078f3e566054fd3cd7b81ec3faadd759c0
Tom Bereknyei <tomberek@gmail.com>
2022-11-29 10:46:17 -0500

    fix: cleaner

--- pkgs/hello-app/default.nix
+++ pkgs/hello-app/default.nix
@@ -4,41 +4,13 @@
   inputs,
   pkgs,
 }:
-let
-  # This can be a builder
-  mkDream = args @ { # {{{
-    pkgs,
-    src,
-    inputs,
-    ...
-  }:
-  let
-    makeOutputs = pkgs: let
-      initD2N = pkgs:
-        inputs.d2n.lib.init {
-          inherit pkgs;
-          config.projectRoot = src;
-        };
-
-      outputs = (initD2N pkgs).dream2nix-interface.makeOutputs ({
-          source = src;
-          settings = [
-            {
-              builder = "granular-nodejs";
-              translator = "package-lock";
-            }
-          ];
-        }
-        // (builtins.removeAttrs args ["src" "inputs" "pkgs"]));
-    in
-      outputs.packages.default;
-    outputs = makeOutputs pkgs;
-  in
-    makeOutputs pkgs;
-in
-  # }}}
-  mkDream {
-    inherit inputs pkgs;
-    src = self;
-    # packageOverrides = {};
-  }
+(inputs.d2n.lib.makeFlakeOutputs {
+  pkgs = pkgs;
+  source = self;
+  settings = [
+    {
+      builder = "granular-nodejs";
+      translator = "package-lock";
+    }
+  ];
+}).packages.${system}.default

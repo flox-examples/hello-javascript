9b7d9cd2c7ebfe26ae599fce88f1f67da4232b7b
Sam Rose <sam@floxdev.com>
2022-11-29 11:57:09 -0500

    docs: testing change and rebuild

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const app = express();
 const port = process.env.APP_PORT ? process.env.APP_PORT : 3000;
 
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello Flox!'));
 app.listen(port, () => console.log(`Listening on port ${port}`));
